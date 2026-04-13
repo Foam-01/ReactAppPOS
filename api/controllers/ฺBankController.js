c24f04a6e4b281faca00704449448d5908f33f3f
Your Name <174691972+Foam-01@users.noreply.github.com>
2026-04-13 14:34:47 +0700

    Fix: Update server listen configuration to bind to all interfaces

--- api/controllers/ฺBankController.js
+++ api/controllers/ฺBankController.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
-const BankModel = require('../models/ฺBankModel');
+const BankModel = require("../models/Temp2");
 
-app.get('/bank/list', async (req, res) => {
-    try {
-        const results = await BankModel.findAll();
-        res.send({ message: 'success', results: results});
-    } catch (e) {
-        res.statusCode = 500;
-        res.send({ message: e.message});
-    }
-})
+app.get("/bank/list", async (req, res) => {
+  try {
+    const results = await BankModel.findAll();
+    res.send({ message: "success", results: results });
+  } catch (e) {
+    res.statusCode = 500;
+    res.send({ message: e.message });
+  }
+});
 
 module.exports = app;
